31ca7ca34f8ebcdab6c515224db7108da64df3ec
India Amos <india@indiamos.com>
2017-06-16 12:57:46 -0400

    Follow Setup and Sequelize Essentials steps

--- app.js
+++ app.js
@@ -3,33 +3,32 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
-const makesRouter = require('./routes');
 const fs = require('fs');
-const path = require('path');
-const mime = require('mime');
 const bodyParser = require('body-parser');
-const socketio = require('socket.io');
+const models = require('./views/models');
+
+// logging middleware
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 // templating boilerplate setup
 app.engine('html', nunjucks.render); // how to render html templates
 app.set('view engine', 'html'); // what file extension do our templates have
-nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
-
-// logging middleware
-app.use(morgan('dev'));
+var env = nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
 
 // body parsing middleware
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
 
-
-// start the server
-const server = app.listen(1337, function() {
-  console.log('listening on port 1337');
-});
-const io = socketio.listen(server);
-
-app.use(express.static(path.join(__dirname, '/public')));
-
-// modular routing that uses io inside it
-app.use('/', makesRouter(io));
+models.User.sync({})
+  .then(function() {
+    return models.Page.sync({})
+  })
+  .then(function() {
+    // make sure to replace the name below with your express app
+    const server = app.listen(3001, function() {
+      console.log('Server is listening on port 3001!');
+    });
+  })
+  .catch(console.error);
